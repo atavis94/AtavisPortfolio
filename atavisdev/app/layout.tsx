dd225dfc063cfefa2f1ac14fbb1de0d358d9388c
Aidan F <foraa008@mymail.unisa.edu.au>
2023-06-30 19:33:10 +0930

    Fix text display

--- atavisdev/app/layout.tsx
+++ atavisdev/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-    <body className={`${inter.className} w-full`}>
+    <body className={`${inter.className} w-full text-white`}>
 
         <NavContainer />
         {children}
