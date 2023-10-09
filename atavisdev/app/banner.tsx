ec5b28a5a772b7d8ce749d01f9c68adad70b571d
Aidan F <foraa008@mymail.unisa.edu.au>
2023-10-09 20:51:00 +1030

    Fix banner sizing

--- atavisdev/app/banner.tsx
+++ atavisdev/app/banner.tsx
@@ -3,7 +3,7 @@ import Atavis from "/resources/atavis.png";
 
 export default function Banner(){
     return(
-        <div className="shadow-md" style={{ minHeight: '88vh' }} id="PurpleBarContainer">  
+        <div className="shadow-md" style={{ minHeight: '90vh' }} id="PurpleBarContainer">  
         <h1 className="text-center subpixel-antialiased text-white text-5xl font-extrabold sm:mx-5 mt-20 md:mt-52">HI, I&apos;M AIDEN. I ENJOY BUILDING THINGS ON THE INTERTUBES.</h1>
           <p className="text-center subpixel-antialiased text-white text-xl font-bold mt-5 my-10" >
             Aspiring software engineer & web developer. Recent graduate of University of South Australia.
