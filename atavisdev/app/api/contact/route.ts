8405dd7e663a28f5e245d4d5b7689720625e8c6e
Aidan F <foraa008@mymail.unisa.edu.au>
2023-10-20 03:47:00 +1030

    Remove console logs

--- atavisdev/app/api/contact/route.ts
+++ atavisdev/app/api/contact/route.ts
@@ -8,7 +8,6 @@ export async function POST(req: Request) {
 
     const { name, email, message } = body;
 
-    // Create email data
     const mailData: nodemailer.SendMailOptions = {
       from: process.env.SITE_ACC,
       to: process.env.DESTINATION_ADD,
@@ -19,14 +18,14 @@ export async function POST(req: Request) {
     return await transporter
     .sendMail(mailData)
     .then((response: nodemailer.SentMessageInfo) => {
-      console.log(response); // Log the response
+      
       return NextResponse.json(
         { error: false, emailSent: true, errors: [], response },
         { status: 200 }
       );
     })
     .catch((error: Error) => {
-      console.error(error); // Log the error
+      
       return NextResponse.json(
         { error: true, emailSent: false, errors: [error.message] },
         { status: 500 }
