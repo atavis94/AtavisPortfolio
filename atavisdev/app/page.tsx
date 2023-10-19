c014f3fe1989ed847828241a710aaf8429b4d9f0
Aidan F <foraa008@mymail.unisa.edu.au>
2023-10-20 03:45:30 +1030

    Spacing

--- atavisdev/app/page.tsx
+++ atavisdev/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <main className="justify-center mt-24 flex flex-col text-white font-semibold">
      
 
-     <Banner />
+      <Banner />
 
       <section id="about">
         <About />
@@ -28,9 +28,9 @@ export default function Home() {
       <section id="contact">
         <ContactForm />
       </section>
+
       <div className="bg-zinc-950">
         <h3 className="text-sm text-center mb-4 mt-4">Built by Aiden Forbes using Next.JS, Tailwind CSS, and hosted on Vercel. Copyright 2023.</h3>
-
       </div>
     </main>
   );
