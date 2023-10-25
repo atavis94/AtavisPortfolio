2d6a6d0504a0ec72326343241f8c9b84f65d338a
Aidan F <foraa008@mymail.unisa.edu.au>
2023-10-25 18:36:05 +1030

    Refactor open modal logic

--- atavisdev/app/showcase.tsx
+++ atavisdev/app/showcase.tsx
@@ -9,49 +9,45 @@ export default function Showcase(){
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedImage, setSelectedImage] = useState(surveyBuilder);
 
-
-  const openModal = (image: StaticImageData) => {
-      setSelectedImage(image);
-      setModalIsOpen(true);
-    };
-
-    return(
-        <div className="bg-zinc-950 text-white py-28 text-center" id="PurpleBarContainer">   
-        <Modal
-            isOpen={modalIsOpen}
-            onRequestClose={() => setModalIsOpen(false)}
-            contentLabel="Full-size Image Modal"
-            className="Modal"
-            overlayClassName="Overlay"
-            >
-            
-            <Image src={selectedImage} alt="Enlarged Version of Selected Image (Varies)" onClick={() => setModalIsOpen(false)}/>
-        </Modal>
-    
-        <h1 className="text-4xl font-extrabold mb-4">SHOWCASE</h1>
-        <div className="purple-bar border-black border-1 mt-4 position-absolute"></div>
-        <div className="flex flex-col sm:flex-row mt-20 items-center md:items-start justify-center">
-          <div className="md:w-1/4 w-3/4 p-4 md:text-start text-center flex flex-col justify-start">
-            <div className="mb-4">
-              <h2 className="text-2xl font-semibold md:text-start">CEIH Heart Failure Survey System</h2>
-            </div>
-            <div>
-              <p className="mb-6">
-                Initiated by the Commission on Exellence and Innovation in  Health&apos;s Heart Failure Subcommitte, this project served as my capstone project as part of my software development degree.                         
-              </p>
-              <p className='mb-6'>
-                The project required constructing a survey system akin to survey monkey which would allow CEIH committe members to build custom surveys to gain specific data from
-                key stakeholders, including clinicians and patients. Additionally, the existing website was rebuilt to improve user friendliness and to allow for the survey system to be integrated.          
-              </p>
- 
-            </div>
+  return(
+      <div className="bg-zinc-950 text-white py-28 text-center" id="PurpleBarContainer">   
+      <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={() => setModalIsOpen(false)}
+          contentLabel="Full-size Image Modal"
+          className="Modal"
+          overlayClassName="Overlay"
+          >
+          
+          <Image src={selectedImage} alt="Enlarged Version of Selected Image (Varies)" onClick={() => setModalIsOpen(false)}/>
+      </Modal>
+  
+      <h1 className="text-4xl font-extrabold mb-4">SHOWCASE</h1>
+      <div className="purple-bar border-black border-1 mt-4 position-absolute"></div>
+      <div className="flex flex-col sm:flex-row mt-20 items-center md:items-start justify-center">
+        <div className="md:w-1/4 w-3/4 p-4 md:text-start text-center flex flex-col justify-start">
+          <div className="mb-4">
+            <h2 className="text-2xl font-semibold md:text-start">CEIH Heart Failure Survey System</h2>
           </div>
-          <div className="md:w-1/4 w-3/4 p-4 mx-10 flex flex-col">
-          <Image src={surveyBuilder} alt="UniSA STEM Logo" className="mb-20 lg:order-0 lg:mx-10 sm:mx-20" onClick={() => openModal(surveyBuilder)}/>
+          <div>
+            <p className="mb-6">
+              Initiated by the Commission on Exellence and Innovation in  Health&apos;s Heart Failure Subcommitte, this project served as my capstone project as part of my software development degree.                         
+            </p>
+            <p className='mb-6'>
+              The project required constructing a survey system akin to survey monkey which would allow CEIH committe members to build custom surveys to gain specific data from
+              key stakeholders, including clinicians and patients. Additionally, the existing website was rebuilt to improve user friendliness and to allow for the survey system to be integrated.          
+            </p>
+
           </div>
         </div>
-        <button className="bg-purple-800 border-black border-2 rounded-xl py-1 px-4" disabled>See More (WIP - Coming Soon)</button>
-        
+        <div className="md:w-1/4 w-3/4 p-4 mx-10 flex flex-col">
+        <Image src={surveyBuilder} alt="UniSA STEM Logo" className="mb-20 lg:order-0 lg:mx-10 sm:mx-20" onClick={() => { 
+          setSelectedImage(surveyBuilder);
+          setModalIsOpen(true)}}/>
+        </div>
       </div>
-    )
+      <button className="bg-purple-800 border-black border-2 rounded-xl py-1 px-4" disabled>See More (WIP - Coming Soon)</button>
+      
+    </div>
+  )
 }
