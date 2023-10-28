dce23201afead1c2abf3d1b49815ae52bd915dc6
Aidan F <foraa008@mymail.unisa.edu.au>
2023-10-28 20:41:04 +1030

    Update banner wording

--- atavisdev/app/banner.tsx
+++ atavisdev/app/banner.tsx
@@ -6,7 +6,7 @@ export default function Banner(){
         <div className="shadow-md" style={{ minHeight: '90vh' }} id="PurpleBarContainer">        
           <h1 className="text-center subpixel-antialiased text-white text-4xl font-extrabold mx-5 mt-20 md:mt-52">HI, I&apos;M AIDEN, AN ASPIRING SOFTWARE ENGINEER.</h1>
           <p className="text-center subpixel-antialiased text-white text-lg font-bold mt-5 mx-5 my-10">
-            Recent software development graduate from the University of South Australia, passionate about crafting digital solutions.
+            Software development graduate from the University of South Australia, passionate about all things programming.
           </p>
 
           <div className="purple-bar border-black border-1 mt-4 position-absolute"></div>
