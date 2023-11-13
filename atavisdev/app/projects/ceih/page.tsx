94be1654456c3daee6d67874a14106d94f756ad9
Aidan F <atavis94@yandex.com>
2023-11-14 04:24:15 +1030

    Improve readability of project page

--- atavisdev/app/projects/ceih/page.tsx
+++ atavisdev/app/projects/ceih/page.tsx
@@ -18,25 +18,23 @@ export default function Ceih() {
         [addQuestion, "Supports adding questions of various types in addition to headings and subsections."],
         [notifications, "Notification system alerts users to new surveys and allows them to be dismissed."]];
     
-    const contentData: [string, string, string[], string[]] = [
+    const contentData: [string, string, string[], string[], string[]] = [
         "UniSA ICT Capstone Project",
         "Commission on Excellence and Innovation in Health - Survey Builder",
         [`The Commission on Exellence and Innovation in Health's Heart Failure Subcommitte initiated a student project to build a web portal
         that would serve as their public interface and also allow CEIH staff to interact with patients and other stakeholders.`,
-        `My primary involvement in the project revolved around integrating a survey system into the existing website, permitting CEIH staff to construct tailored surveys 
-            to gather insights from crucial stakeholders, including clinicians and patients.`,
-        `When I took the project, the initiative had been underway for over a year, with the previous team incorporating Umbraco's content management system and relying on the .NET Framework.`,
-        `The website required a complete rebuild as the existing codebase was undocumented, messy, and difficult to work with. I saw this as a necessity to 
-        ensure the future success of the project given that it is an ongoing student project. While remaining faithful to the original design, the 
-        original site was rebuilt using technology and frameworks familiar to UniSA's software engineering students to ensure that future students would have no trouble working with the codebase.`,
-        `Upon handing off the project, I wrote extensive documentation and a detailed user manual, including tutorials on particular challenges such as working with Umbraco, 
-        ensuring that future students could hit the ground running. The end product was a success and the client was very pleased with the result.`,
-        `Unfortunately, the project is an ongoing work in progress and is not available for live interaction.`],
-        [".Net Framework", "C#", "JavaScript", "Umbraco CMS", "SQLite", "Bootstrap V5", "CSS", "HTML"]
+        `The initiative had been underway for over a year, with the previous team incorporating Umbraco's content management system and relying on the .NET Framework.`,
+        `My primary involvement revolved around building and implementing a survey system into the website, permitting CEIH staff to construct tailored surveys to gather insights 
+            from crucial stakeholders, including clinicians and patients.`,
+        `The existing site required a complete rebuild as the codebase was undocumented, messy, and difficult to work with. I saw this as a necessity to 
+        ensure the future success of the project given that it is an ongoing student project. 
+         Remaining faithful to the original design, the site was rebuilt using technology and frameworks familiar to UniSA's software engineering students to ensure that future students would have no trouble working with the codebase.`,
+        `The end product was a success and the client was very pleased with the result.`],
+        [".Net Framework", "C#", "JavaScript", "Umbraco CMS", "SQLite", "Bootstrap V5", "CSS", "HTML"],
+        [`Unfortunately, the project is an ongoing work in progress and is not available for live interaction.`]
     ]
 
 return ( 
-
     <div className="md:mt-18 pb-20 lg:flex-row justify-center shadow-lg">
         <Carousel imageContent={imageProps} />        
         <ProjectContent content={contentData} />      
--- atavisdev/app/projects/projectContent.tsx
+++ atavisdev/app/projects/projectContent.tsx
@@ -1,25 +1,37 @@
 
-// Title, SubTitle, MainDescription, Notes
-export default function ProjectContent({content}: {content: [string, string, string[] | string, string[]]}){
+// Title, SubTitle, MainDescription, AppliedSkills, LiveAccess
+export default function ProjectContent({content}: {content: [string, string, string[] | string, string[], string[]]}){
     return(
         <div className="bg-alt border-t border-zinc-950 border-l border-r py-9 md:mx-72 mx-6 rounded-lg text-center" id="PurpleBarContainer">
-            <div className="bg-zinc-950 mx-10 rounded-lg">
-
+            <div className="bg-zinc-950 md:mx-10 mx-9 rounded-lg pb-6">
             <h1 className="text-4xl pt-4 text-center font-extrabold mb-4">{content[0]}</h1>
-            <h3 className="font-semibold text-lg pb-4 mb-1">{content[1]}</h3>
+            <h3 className="mx-4 md:mx-0 font-semibold text-lg pb-4 mb-1">{content[1]}</h3>
             <div className="purple-bar border-black border-1 my-4 position-absolute"></div>
-            {typeof content[2] === 'string' ? <p className="mx-6 font-semibold text-md my-6 text-gray-400">{content[2]}</p> : content[2].map((paragraph, index) => ( 
-                <p className="mx-6 font-semibold text-md my-3 text-gray-400 pb-4" key={index}>{paragraph}</p> ))}
             </div>
-            <div className="bg-zinc-950 rounded-lg py-6 flex flex-wrap justify-center mx-10">
-                {content[3].map((skill, index) => (
-                    <span key={index} className="tag my-1 mx-1 font-bold border-black border-2">{skill}</span>
-                ))}
-
+            <div className="flex flex-col sm:flex-row mt-9 items-center md:items-start justify-center">
+                <div className="md:w-1/2 w-5/6 p-4 md:text-start text-center flex flex-col justify-start bg-zinc-950 rounded-lg md:ml-10">
+                    {typeof content[2] === 'string' ? <p className="mx-6 font-semibold text-md my-6 text-gray-400">{content[2]}</p> : content[2].map((paragraph, index) => ( 
+                    <p className="mx-6 font-semibold text-md my-1 text-gray-400 pb-4" key={index}>{paragraph}</p> ))}
+                </div>       
+                <div className="md:w-1/2 w-5/6 p-4 mx-10 mt-4 md:mt-0 flex flex-col bg-zinc-950 rounded-lg">
+                    <div className="bg-alt rounded-lg">
+                        <h3 className="mt-4 font-bold text-xl">Applied Skills</h3>                
+                        <div className="rounded-lg py-6 flex flex-wrap justify-center m-2">                           
+                            {content[3].map((skill, index) => (
+                                <span key={index} className="tag my-1 mx-1 font-bold border-black border-2">{skill}</span>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="bg-alt mt-4 rounded-lg">
+                    <h3 className="mt-4 font-bold text-xl">Live Access</h3>                
+                        <div className="rounded-lg py-6 flex flex-wrap justify-center m-2 ">
+                            {content[4].map((info, index) => (                        
+                                <p key={index} className="font-semibold text-md">{info}</p>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
-           
-
-
         </div>
     )
 }
