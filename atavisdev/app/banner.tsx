efea097fa6eef81f683d9a08f3d26003b6a20b1e
Aidan F <atavis94@yandex.com>
2023-12-21 17:32:54 +1030

    Reverse change

--- atavisdev/app/banner.tsx
+++ atavisdev/app/banner.tsx
@@ -5,14 +5,16 @@ export default function Banner(){
     return(
         <div className="shadow-md" style={{ minHeight: '90vh' }} id="PurpleBarContainer">        
           <h1 className="text-center subpixel-antialiased text-white text-5xl font-extrabold mx-5 mt-20 md:mt-52">
-            Atavis Web Development
+            HI, I&apos;M AIDEN, AN ASPIRING SOFTWARE ENGINEER.
           </h1>
           <p className="text-center subpixel-antialiased text-white text-lg font-bold mt-5 mx-5 my-10">
-          Custom websites done your way
+            Software development graduate from the University of South Australia, passionate about all things programming.
           </p>
 
           <div className="purple-bar border-black border-1 mt-4 position-absolute"></div>
- 
+          <div className="flex items-center justify-center mt-10 z-50" >
+            <Image src={Atavis} alt="Atavis" width={200} height={200} className="rounded-full h-40 w-40 border-black border-2 shadow-lg bg-cover mb-10" id="PurpleBarContainer"/> 
+          </div>  
         </div>
     )
 }
